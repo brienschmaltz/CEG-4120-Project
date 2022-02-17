884610e3b756a2b6d01bf3095a0a70eb173d4d18
Makhmud to the M <makhmudov.5@wright.edu>
2022-02-16 22:47:50 -0500

    Added Progress Bar - More work done cleaning up the program code and adding some print messages.

--- Mak's Work/Project-Work/main.py
+++ Mak's Work/Project-Work/main.py
@@ -1,39 +1,36 @@
+from removeLabelCopy import *
 from email.mime import image
 from PIL import Image
+from tqdm import tqdm
+
+import time
 import glob
 import os, os.path
-
 import cv2 as cv2
 import numpy as np
-from removeLabelCopy import *
 
-def main(): # Driver
-    ######################################### MAIN #########################################
-    #reads in image
-    ## image = "Q016316C1520U02.tif" #1
+def main(): #                                                       *** MAIN ***
 
 
+    mainMenu() # START MAIN MENU
 
-
-    mainMenu() 
-
-# Terminal I/!O 
-def mainMenu(): # Begins Calling Funtions 
-    print("*** Image Cleanup Tool V.1.0 ***")                   # Menu Screen
+def mainMenu():                                                     # Start Program
+    print("*** Image Cleanup Tool V.1.0 ***")                       # Menu Screen
 
     images_retrieved = []                                           # Array of Images Extracted from Folder
     valid_imageExtensions = [".tif", ".png"]                        # Approved Image extentions used later to verify output files
 
     directoryInput = input ("Enter Image/s Retrieval Directory:")   # User Input of Retrieval Directory
+    folderInspection(directoryInput)
     directoryInput = directoryInput + '\*'                          # the addition of '*' symbol is proper semantics when accessing files in folders
-#    directoryOutput = input("Enter Image/s Output Directory:")      # User Inputs directory to store output images
-
     imageRetrieval(directoryInput,images_retrieved)                 # Sending directyInput & the array to store the images in to the image retrieval function
 
     imageToCV2(images_retrieved)                                    # After imageRetrieval completion program sends imported images array to CV2 editor
 
+
 def imageToCV2(images_retrieved):
-    for i in images_retrieved:                                      #  traversing through the image loop to find each image and pass forward to removeLabel
+    imageCounter = 1
+    for i in tqdm(images_retrieved):                                      #  traversing through the image loop to find each image and pass forward to removeLabel
         print("image " + i) # DEBUG PURPOSES
 
         #for Desktop !!CHANGE THIS FOR YOUR COMPUTER!!
@@ -52,7 +49,7 @@ def imageToCV2(images_retrieved):
 
         #for desktop !!CHANGE THIS FOR YOUR COMPUTER!!
 
-        cv2.imwrite("D:\GITHUB\CEG-4121-Project\Mak's Work\OutputImages\Result" + str(image) + ".jpg", result)
+        cv2.imwrite("D:\GITHUB\CEG-4121-Project\Mak's Work\OutputImages\result" + str(image) + ".jpg", result)
 
         #for laptop
         #cv2.imwrite("C:/Users/ashto/OneDrive/Desktop/CEG 6120 Managing the Software Process/Group E Project/Output Images/" + "MASK" + image, mask)
@@ -61,29 +58,27 @@ def imageToCV2(images_retrieved):
         cv2.destroyAllWindows()
 
         print("Image: " + i + " Succesful")
+        imageCounter += 1
+        time.sleep(0.3)
 
-def folderInspection(directoryInput):
+
+def folderInspection(directoryInput):                               # Function designed to check if folder exists
     print("Checking Folder Path (" + directoryInput + ")...")
 
-    if os.path.exists(directoryInput):
+    if os.path.exists(directoryInput):                              # Does directory exist?
         print("File Path Exists") 
     else:
-        print("File Path Does Not Exist: Would You Like to Enter a new directiory?")    #can not write there
+        print("File Path Does Not Exist: Would You Like to Enter a new directiory?")    # Path Does NOT EXIST
+        newDirectory = input
+        folderInspection(newDirectory)                              # If User wants to enter new directory, program will send the new directory to be inspected
+
 
 def imageRetrieval(directoryInput, images_retrieved):               # Function Retrieves Images from Directory and Stores In 'images_retrieved' array
-
+    imageIntegrity(images_retrieved)                                # Sends Retrieved Images to get checked for file integrity
     for f in glob.iglob(directoryInput):
         print('found ' + f)
         images_retrieved.append(f)                                  # Adds Image to Array
-        #ImportedImage = Image.open(f)                               # Retrieving The Image(ImportedImage) using Image.Open(filename)
-        #ImportedImage.show()                                        # Shows Images that it is opening
 
-    # imageIntegrity(images_retrieved)                                # Sends Retrieved Images to get checked for file integrity
-
-#        if ImportedImage is not None:
-#            imageIntegrity(images_retrieved)                        # If image exists check its file type
-#            images_retrieved.append(ImportedImage)                  # Append the image into (images_retrieved) arr
-        
 
 def imageIntegrity(images_retrieved):
     print("Now Checking File Integrity")                            # Debug Purposes
@@ -102,9 +97,11 @@ def imageIntegrity(images_retrieved):
         elif split_extension != ".jpg":
             print("File Integrity: It is not .jpg!")
 
+
 def showImage(images_retrieved):                                    # Shows user current image
     for images in images_retrieved:
         images.show()                                               # Shows image received / Debug purposes
+
 
 if __name__ == "__main__":
     main()
